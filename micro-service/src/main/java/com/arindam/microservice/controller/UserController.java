c3c07874dbb01c53d0fe43c8baeb173edc2cd272
travelingdeveloper12 <dutta_ari@yahoo.in>
2020-10-01 02:25:18 +0530

    User implementation Service

--- micro-service/src/main/java/com/arindam/microservice/controller/UserController.java
+++ micro-service/src/main/java/com/arindam/microservice/controller/UserController.java
@@ -1,8 +1,23 @@
 package com.arindam.microservice.controller;
 
+import com.arindam.microservice.entity.User;
+import com.arindam.microservice.repository.UserDaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(value = "/microservice/user")
 public class UserController {
 
+    @Autowired
+    private UserDaoService userDaoService;
+
+    @GetMapping(path = "/get/all")
+    public List<User> getAllUsers() {
+        return userDaoService.getAllUsers();
+    }
 }
--- micro-service/src/main/java/com/arindam/microservice/repository/UserDaoService.java
+++ micro-service/src/main/java/com/arindam/microservice/repository/UserDaoService.java
@@ -1,4 +1,15 @@
 package com.arindam.microservice.repository;
 
+import com.arindam.microservice.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class UserDaoService {
+    public List<User> getAllUsers() {
+        return new ArrayList<>();
+    }
+
 }
